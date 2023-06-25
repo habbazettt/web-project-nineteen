3cf33f6e58d8f9cc5bf92572b0fa5279ca097643
habbaza <habbazahubbal@gmail.com>
2023-06-25 19:59:50 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+// MENU SHOW
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //!! ketika klik tiap nav__link , hapus class show-menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper (".home-swiper", {
+    spaceBetween:30,
+    loop: 'true',
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    //!! ketika Scroll > 50 vh, tambah class scroll-header ke tag header
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper (".new-swiper", {
+    centeredSlide: true,
+    slidePerView: 'auto',
+    spaceBetween: 16,
+    loop: 'true',
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+    //!! Ketika scroll > 460vh, tambah show-scroll ke tag
+    if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
